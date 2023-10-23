e07a67ba525bf0f67d1fcc10f512ec0fd972d2de
Dominik Kolarsky <kolarskydominik@gmail.com>
2023-10-23 10:39:37 +0200

    fix window.fn

--- src/main.tsx
+++ src/main.tsx
@@ -5,8 +5,8 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BookingContextProvider } from './utils/providers/BookingContextProvider.tsx';
 
-const widgetName = 'calendar-app';
-const widgetDiv = document.getElementById(widgetName);
+// const widgetName = 'calendar-app';
+// const widgetDiv = document.getElementById(widgetName);
 
 // ReactDOM.createRoot(widgetDiv!).render(
 //   <React.StrictMode>
@@ -20,7 +20,7 @@ const widgetDiv = document.getElementById(widgetName);
   ReactDOM.createRoot(document.getElementById(containerId)!).render(
     <React.StrictMode>
       <BookingContextProvider>
-        <App domElement={widgetDiv!} />
+        <App domElement={document.getElementById(containerId)!} />
       </BookingContextProvider>
     </React.StrictMode>
   );
