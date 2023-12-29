b8ee9fd93a730d6cf04ffa27932f50ee0aa2ef66
Dominik Kolarsky <kolarskydominik@gmail.com>
2023-12-29 11:03:03 +0100

    update render

--- src/main.tsx
+++ src/main.tsx
@@ -3,18 +3,35 @@ import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import { BookingContextProvider } from './utils/providers/BookingContextProvider.tsx';
 
-const widgetName = 'calendar-app';
-const widgetDiv = document.getElementById(widgetName);
+const calendarWidgetId = 'calendar-app';
+const calendarWidgetDiv = document.getElementById(calendarWidgetId);
 
-//if (import.meta.env.DEV) {
-  ReactDOM.createRoot(widgetDiv!).render(
+if (import.meta.env.DEV) {
+  ReactDOM.createRoot(calendarWidgetDiv!).render(
     <React.StrictMode>
       <BookingContextProvider>
-        <App domElement={widgetDiv!} />
+        <App domElement={calendarWidgetDiv!} />
       </BookingContextProvider>
     </React.StrictMode>
   );
-// } else {
+}
+
+const renderCalendarWidget = (containerId: string) => {
+  const containerDiv = document.getElementById(containerId);
+
+  return ReactDOM.createRoot(containerDiv!).render(
+    <React.StrictMode>
+      <BookingContextProvider>
+        <App domElement={containerDiv!} />
+      </BookingContextProvider>
+    </React.StrictMode>
+  );
+};
+
+export { renderCalendarWidget };
+
+// * old
+//else {
 //   (window as any).renderCalendar = (containerId: string) => {
 //     ReactDOM.createRoot(document.getElementById(containerId)!).render(
 //       <React.StrictMode>
